c534a9b2c9a51ffbb3a0df755622e716b921ad8a
PatheticHyntax <79538703+PatheticHyntax@users.noreply.github.com>
2025-10-03 11:50:09 -0400

    Add files via upload

---
+++ Canal.java
@@ -0,0 +1,13 @@
+public class Canal {
+    
+    private String _nome;
+    
+    public Canal(String nome) {
+        this._nome = nome;
+    }
+
+    public String get_nome() {
+        return _nome;
+    }
+
+}
